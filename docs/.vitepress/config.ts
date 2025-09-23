7a6dd509c5265dc24cca99b647faedb8d568c48a
LiuYu <69404608+syskuku@users.noreply.github.com>
2025-09-23 21:57:42 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
   base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
 
   lang: 'zh-CN',
-  title: '茂茂物语',
-  description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
+  title: 'iMikufans未来网络',
+  description: '没什么',
   head,
 
   lastUpdated: true,
@@ -41,8 +41,8 @@ export default defineConfig({
     socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],
 
     footer: {
-      message: '如有转载或 CV 的请标注本站原文地址',
-      copyright: 'Copyright © 2019-present maomao',
+      message: '模板链接',
+      copyright: 'Copyright © 2025 SYSKUKU研究部',
     },
 
     lastUpdated: {
